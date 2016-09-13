ddbb8f673758fcab2cffdf8b0667ddbd167f8520
chenkechao <chenkechao123@163.com>
2016-09-14 07:45:31 +0800

    init

--- webapp/src/js/vendor.js
+++ webapp/src/js/vendor.js
@@ -1,3 +1,7 @@
+require('../../../node_modules/angular-material/angular-material.min.css');
 
 require('jquery');
 require('angular');
+require('angular-animate');
+require('angular-aria');
+require('angular-material');
