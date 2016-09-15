777e8684285eca2d8e83361e735660c8afce06c8
chenkechao <chenkechao520>
2016-09-15 21:38:32 +0800

    gh-pages

--- webapp/src/js/login.js
+++
@@ -1,2 +0,0 @@
-require('../css/login.css');
-require('jquery');
--- webapp/src/js/vendor.js
+++
@@ -1,9 +0,0 @@
-require('../../../node_modules/angular-material/angular-material.min.css');
-/* Styles */
-require('../css/style.scss');
-
-import jquery from 'jquery';
-import angular from 'angular';
-import angular_animate from 'angular-animate';
-import angular_aria from 'angular-aria';
-import angular_meterial from 'angular-material';
