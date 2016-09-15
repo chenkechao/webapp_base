6bc92884e4972b2d6c3b3870c2ecdd5d206c7acd
chenkechao <chenkechao520>
2016-09-16 01:00:29 +0800

    gh-page

--- webapp/src/js/index.js
+++ webapp/src/js/index.js
@@ -5,13 +5,13 @@ export default angular.module('app',['ngMaterial',Users.name]).config(
     ($mdIconProvider, $mdThemingProvider) => {
         // Register the user `avatar` icons
         $mdIconProvider
-            .defaultIconSet("/dist/avatars.svg", 128)
-            .icon("menu", "/dist/menu.svg", 24)
-            .icon("share", "/dist/share.svg", 24)
-            .icon("google_plus", "/dist/google_plus.svg", 24)
-            .icon("hangouts", "/dist/hangouts.svg", 24)
-            .icon("twitter", "/dist/twitter.svg", 24)
-            .icon("phone", "/dist/phone.svg", 24);
+            .defaultIconSet("webapp/dist/avatars.svg", 128)
+            .icon("menu", "webapp/dist/menu.svg", 24)
+            .icon("share", "webapp/dist/share.svg", 24)
+            .icon("google_plus", "webapp/dist/google_plus.svg", 24)
+            .icon("hangouts", "webapp/dist/hangouts.svg", 24)
+            .icon("twitter", "webapp/dist/twitter.svg", 24)
+            .icon("phone", "webapp/dist/phone.svg", 24);
 
         $mdThemingProvider.theme('default')
             .primaryPalette('brown')
